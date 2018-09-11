e14328a9ec78b7b194b9b038ce1387fd9c21f018
Milouz2 <emilien.wacrenier@epsi.fr>
2018-09-11 20:53:26 +0200

    pauffiner inscri

--- public/pages/Controller/inscription.php
+++ public/pages/Controller/inscription.php
@@ -6,38 +6,41 @@ include '../views/shared/bdd.php';
 if ($_POST['password'] != $_POST['passwordBis']){
     header("location: ../views/account.php");
 }
-
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$mail = $_POST['mail'];
-$tel = $_POST['tel'];
-$age = $_POST['age'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-
-// Convertion de permis en 1-0
-if ($_POST['permis'] == true){ 
-    $permis = 1;
-}
 else{
-    $permis = 0;
-}
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $mail = $_POST['mail'];
+    $tel = $_POST['tel'];
+    $age = $_POST['age'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+
+    // Convertion de permis en 1-0
+    if ($_POST['permis'] == true){ 
+        $permis = 1;
+    }
+    else{
+        $permis = 0;
+    }
+        
+
+    // Requête insertion d'utilisateur
+    $req = $conn->prepare("INSERT INTO utilisateur (nom, prenom, tel, mail, password, admin, bde, age, permis, promo) VALUES (:nom, :prenom, :tel, :mail, :password, 0, 0, :age, :permis, :promo) ");
+    $req->execute(array(
+        'mail' => $mail,
+        'nom' => $nom,
+        'prenom' => $prenom,
+        'tel' => $tel,
+        'age' => $age,
+        'password' => $password,
+        'permis' => $permis,
+        'promo' => $_POST['promo']
+    ));
+
     
+    header("location: ../../index.php");
 
-// Requête insertion d'utilisateur
-$req = $conn->prepare("INSERT INTO utilisateur (nom, prenom, tel, mail, password, admin, bde, age, permis, promo) VALUES (:nom, :prenom, :tel, :mail, :password, 0, 0, :age, :permis, :promo) ");
-$req->execute(array(
-    'mail' => $mail,
-    'nom' => $nom,
-    'prenom' => $prenom,
-    'tel' => $tel,
-    'age' => $age,
-    'password' => $password,
-    'permis' => $permis,
-    'promo' => $_POST['promo']
-));
+    }
 
- 
-header("location: ../../index.php");
 
 
