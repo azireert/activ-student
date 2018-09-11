18c8fd584eb2fb3edff66f8b2c59325a709d7811
Milouz2 <emilien.wacrenier@epsi.fr>
2018-09-11 14:49:54 +0200

    Les sessions

--- public/pages/Controller/login.php
+++ public/pages/Controller/login.php
@@ -1,21 +1,29 @@
-<?php 
-include '../views/shared/bdd.php';
 
-// Récupération de l'id et du password de l'utilisateur voulant se connecter
-$checkMailR = $conn->prepare('SELECT password, id FROM utilisateur WHERE mail = :mail');
-$checkMailR -> execute(array(
-    'mail' => $_POST['name_account']
-));
-$checkMailF = $checkMailR->fetch();
+    <?php
+    session_start();
+    include '../views/shared/bdd.php';
 
-// Le pseudo éxiste t'il?
-if ($checkMailF['id'] AND $_POST['password'] == $checkMailF['password']){
-    header("location: ../views/home.php");
-    
-}
-else{
-    header("location: ../../index.php");
-}
+    // Récupération de l'id et du password de l'utilisateur voulant se connecter
+    $checkMailR = $conn->prepare('SELECT password, id, nom, prenom FROM utilisateur WHERE mail = :mail');
+    $checkMailR -> execute(array(
+        'mail' => $_POST['name_account']
+    ));
+    $checkMailF = $checkMailR->fetch();
+
+    // Le pseudo éxiste t'il?
+    if ($checkMailF['id'] AND $_POST['password'] == $checkMailF['password']){
+        //header("location: ../views/home.php");
+        
+        $_SESSION["id"] = $checkMailF['id'];
+        
+        
+    }
+    else{
+        header("location: ../../index.php");
+    }
 
 
-?>
+    ?>
+
+
+
