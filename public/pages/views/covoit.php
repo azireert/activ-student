0e163015b94b09bea382d5826c1215f49fe29fb7
Milouz2 <emilien.wacrenier@epsi.fr>
2018-09-12 14:19:49 +0200

    covoit

--- public/pages/views/covoit.php
+++ public/pages/views/covoit.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php include('shared/bdd.php'); ?>
 <html>
 
 <head>
@@ -9,6 +10,17 @@
 	<?php include('shared/navbar.php'); ?>
 
 
+    <?php // Requête lire les covoit
+    $covoit = $conn->prepare("SELECT * FROM covoit, utilisateur WHERE covoit.auteur = utilisateur.id ORDER BY date desc");
+    $covoit->execute();
+
+    //Affichage des données
+    while($covoitF = $covoit->fetch()){
+        echo $covoitF['date'] . " === " . $covoitF['description'] . " === " . $covoitF['nom'] . " === Depart: " . $covoitF['depart'] . " === Arrivé: " . $covoitF['arrivee'] ;
+        echo  "</br>";
+    }
+
+    ?>
 
     <?php include('shared/footer.php'); ?>
 </body>
