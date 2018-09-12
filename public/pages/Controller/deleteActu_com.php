2617a314e2cc859406d8266e9921d9071ba8ee0e
Milouz2 <emilien.wacrenier@epsi.fr>
2018-09-12 18:46:29 +0200

    b

--- public/pages/Controller/deleteActu_com.php
+++ public/pages/Controller/deleteActu_com.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 include '../views/shared/bdd.php';
 
 // Delete de commentaire sur actu
 $deleteActu_com = $conn-> prepare("DELETE FROM actu_com WHERE id = :id");
 $deleteActu_com->execute(array(
-    'id' => $_POST['id']
+    'id' => $_SESSION['id']
 ));
 
 header("location: ../views/home.php");
