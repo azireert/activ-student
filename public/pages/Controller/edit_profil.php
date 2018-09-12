622e685a438d14a93cb9f9e69a34023ea586687a
tibo <thibault.dasi@gmail.com>
2018-09-12 11:33:26 +0200

    edit profil

--- public/pages/Controller/edit_profil.php
+++ public/pages/Controller/edit_profil.php
@@ -1,69 +1,31 @@
 <?php
-//login.php
-require __DIR__.'/../../../vendor/autoload.php';
-use Symfony\Component\Yaml\Yaml;
+    session_start();
+    include '../views/shared/bdd.php';
 
-$db = Yaml::parse(file_get_contents('../../../config/db.yml'));
+    if($_POST) {   //Creation des variables si il y a un POST
 
-//lecture
-$config = new \Doctrine\DBAL\Configuration();
-//..
-$connectionParams = array(
-    'dbname' => $db['name'],
-    'user' => $db['login'],
-    'password' => $db['password'],
-    'host' => $db['host'],
-    'driver' => 'pdo_mysql',
-);
-$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
-
-if($_POST) {   //Creation des variables du profil si il y a un POST
-
-    $id_customer = $_POST['id_customer'];
-    $password = $_POST['password'];
+    $id = $_SESSION['id'];
 
     $sql = "SELECT*
-    FROM customer
-    WHERE id_customer = ".$conn->quote($id_customer)."";
+    FROM utilisateur
+    WHERE id = ".$conn->quote($id)."";
     $stmt = $conn->executeQuery($sql);
     $row = $stmt->fetch();
     // var_dump($row['Password']);
     // echo $row;
-    if (password_verify($password, $row['password'])) {
 
-        $id_customer = $_POST['id_customer'];
-        $title = $_POST['title'];
-        $gender = $_POST['gender'];
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $nationality = $_POST['nationality'];
-        $email = $_POST['email'];
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        $birthdate = $_POST['birthdate'];
-        $phone_number = $_POST['phone_number'];
-        $id_type = $_POST['id_type'];
-        $id_number = $_POST['id_number'];
+    $id_customer = $_POST['tel'];
+    $title = $_POST['promo'];
+    $gender = $_POST['age'];
+    $first_name = $_POST['permis'];
         
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            if (isset($_POST['title']) && isset($_POST['gender']) && isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['nationality']) && isset($_POST['email']) && isset($_POST['birthdate']) && isset($_POST['phone_number']) && isset($_POST['id_type']) && isset($_POST['id_number']) && isset($_POST['id_customer'])){
+        if (isset($_POST['tel']) && isset($_POST['promo']) && isset($_POST['age']) && isset($_POST['permis'])){
 
-                // version protégée contre les injections sql
-                $sql =    //insertion des donnees dans la base de donnee
-                'UPDATE profil_customer SET title="'.$_POST['title'].'" , gender = "'.$_POST['gender'].'", first_name = "'.$_POST['first_name'].'", last_name = "'.$_POST['last_name'].'", nationality = "'.$_POST['nationality'].'", email = "'.$_POST['email'].'", birthdate = "'.$_POST['birthdate'].'", phone_number = "'.$_POST['phone_number'].'", id_type = "'.$_POST['id_type'].'", id_number = "'.$_POST['id_number'].'" WHERE id_customer = "'.$_POST['id_customer'].'" ';
-                // var_dump($sql);
-                $conn->query($sql);
-                header('Location: ../view/customer_html.php');    //envoi sur une page  
-            }
+            // version protégée contre les injections sql
+            $sql =    //insertion des donnees dans la base de donnee
+            'UPDATE utilisateur SET tel="'.$_POST['tel'].'" , promo = "'.$_POST['promo'].'", age = "'.$_POST['age'].'", permis = "'.$_POST['permis'].'"';
+            // var_dump($sql);
+            $conn->query($sql);
+            header('Location: ../views/profil.php');    //envoi sur une page  
         }
-
-        else{     // Si False, message d'erreur
-        echo("Your email is not a valid email address");
-        }
-
     }
-
-    else{
-        echo ("Your ID Customer or your password is Wrong");
-    }
-    
-}
