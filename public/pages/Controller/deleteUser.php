074aac87f55823085a1627f17384ade2ea88248d
Milouz2 <emilien.wacrenier@epsi.fr>
2018-09-13 10:08:55 +0200

    bonjour

--- public/pages/Controller/deleteUser.php
+++ public/pages/Controller/deleteUser.php
@@ -3,6 +3,20 @@ include '../views/shared/bdd.php';
 session_start();
 
 
+// Les actu liées à l'utilisateur
+$listeActu = $conn->prepare("SELECT * FROM actu WHERE auteur = :id");
+$listeActu->execute(array(
+    'id' => $_POST['delete']
+));
+
+
+// Parcours des actu liées à l'utilisateur
+while ($listeActuF = $listeActu->fetch()){
+    $deleteActu_com = $conn->prepare("DELETE FROM actu_com WHERE id_actu_com = :id");
+    $deleteActu_com->execute(array(
+        'id' => $listeActuF['id_actu']
+    ));
+}
 
 //header("location: ../views/listeUser.php");
 
--- public/pages/views/listeUser.php
+++ public/pages/views/listeUser.php
@@ -22,6 +22,8 @@ if($admin != 1 or !$_SESSION['id']){
     $listeUser = $conn->prepare("SELECT * FROM utilisateur");
     $listeUser->execute();
 
+    $array = array();
+
     // Affichage des utilisateurs
     while ($listeUserF = $listeUser->fetch()){
         echo $listeUserF['mail'] . "</br>";
@@ -32,10 +34,12 @@ if($admin != 1 or !$_SESSION['id']){
         echo $listeUserF['tel'] . "</br>";
         echo $listeUserF['permis'] . "</br>";
         ?>
+
+        
         <!-- Suppression d'utilisateur (bouton) -->
         <form action="../Controller/deleteUser.php" method="post">
-            <div class="form-group"><button class="btn btn-primary" type="submit" name="delete"> Supprimer ce compte </button></div>
-            <?php $_SESSION['userDelete'] = $listeUserF['id']; ?>
+        <div class="form-group"><button class="btn btn-primary" value= <?php echo $listeUserF['id']?> type="submit" name="delete"> Supprimer ce compte </button></div>
+            
         </form>
         <form action="../Controller/editUser.php" method="post">
             <div class="form-group"><button class="btn btn-primary" type="submit" name="update"> Modifier ce compte </button></div>
